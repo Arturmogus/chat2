99f1ee290298ee4b3c33e298364e2e9b11324cd2
Arturmogus <134745658+Arturmogus@users.noreply.github.com>
2023-10-05 19:20:18 -0400

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDzVZvkmi04yRL6wWD6G1NUFYpuuupc-Ok",
+    authDomain: "project101-8d868.firebaseapp.com",
+    databaseURL: "https://project101-8d868-default-rtdb.firebaseio.com",
+    projectId: "project101-8d868",
+    storageBucket: "project101-8d868.appspot.com",
+    messagingSenderId: "743891776032",
+    appId: "1:743891776032:web:94150d59fba9f98d460245",
+    measurementId: "G-KP826NRBTG"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
